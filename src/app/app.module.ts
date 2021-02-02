9efe59cdb8aeac384ec0a35c6a5f6471b9c85883
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-02 19:57:19 +0300

    added a date count pipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteComponent } from './quote/quote.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { QuoteComponent } from './quote/quote.component';
     QuoteFormComponent,
     QuoteDetailsComponent,
     QuoteComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
