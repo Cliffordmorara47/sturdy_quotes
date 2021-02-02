ca699d3ff6ef94d68a47ae2fa17913e9f8b41dda
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-02 19:59:13 +0300

    cleared errors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   Quotes: Quote[] = [
-    new Quote(0, "Tough Moments", "Don't worry about the things or people that you have lost in your life, things and people who are meant to be with you are with you", "Clifford Morara", 0, 0, new Date(2019,3,4)),
+    new Quote(0, "Tough Moments", "Don't worry about the things or people that you have lost in your life, things and people who are meant to be with you are with you", "Clifford Morara", 0, 0, new Date(2010,3,23)),
     new Quote(1, "Tough Moments", "When you are broken, its very hard to feel special", "Martin Garrix", 0, 0, new Date(2018,6,7)),
     new Quote(2, "Encouragement", "I would rather die on my feet than live on my knees", "Nelson Mandela", 0, 0, new Date(2020,7,7)),
     new Quote(3, "Heed", "The world is a place that has a great risks and fairly great opportunities", "George Bush", 0, 0, new Date(2017,4,23)) 
